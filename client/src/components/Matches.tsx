bed239025b70b7e523356ecd9f96d2ad4cb28bdc
Cat McGee <helloworld@mcgee.cat>
2023-05-13 01:17:23 +0100

    frontend logic for matches

--- client/src/components/Matches.tsx
+++ client/src/components/Matches.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { ALL_MATCHES_QUERY } from '../graphql/swipe';
+import { useProfileQuery, ProfileQueryVariables } from '../graphql/generated';
 import useLensUser from '@/lib/auth/useLensUser';
-import { client } from '../lib/apollo.js';
 
-function Matches() {
+interface MatchedProfileProps {
+  matchId: string;
+}
+
+const MatchedProfile: React.FC<MatchedProfileProps> = ({ matchId }) => {
+  const { data, error, isLoading } = useProfileQuery({
+    request: { profileId: matchId },
+  });
+
+  if (isLoading) return <p>Loading profile...</p>;
+  if (error) return <p>Error loading profile</p>;
+
+  return (
+    <li className="bg-white rounded-lg shadow px-5 py-3 my-3 flex items-center justify-between">
+      <div className="text-lg font-semibold text-gray-700">Match ID: {matchId}</div>
+      <div className="text-sm text-gray-500">{data ? data?.profile?.name : 'Loading...'}</div>
+    </li>
+  );
+};
+
+const Matches: React.FC = () => {
   const { profileQuery } = useLensUser();
   const { data, loading, error } = useQuery(ALL_MATCHES_QUERY, {
     variables: {
@@ -15,24 +35,20 @@ function Matches() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  return ( 
-
-    <div>
-      <h1>My Matches</h1>
-      {data.allMatches && data.allMatches.length > 0 ? (
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold text-gray-900 mt-10 mb-5">Your Matches</h1>
+      {data?.allMatches && data.allMatches.length > 0 ? (
         <ul>
-         {data?.allMatches && data.allMatches.map((matchId: any) => (
-  <li key={matchId.toString()}>
-    Match ID: {matchId}
-  </li>
-))}
-
+          {data.allMatches.map((matchId: any) => (
+            <MatchedProfile key={matchId} matchId={matchId} />
+          ))}
         </ul>
       ) : (
-        <p>No matches yet. Get swiping!</p>
+        <p className="text-gray-600">No matches yet.</p>
       )}
     </div>
   );
-}
+};
 
 export default Matches;
--- client/src/graphql/generated.ts
+++ client/src/graphql/generated.ts
@@ -4786,6 +4786,13 @@ export type DefaultProfileQueryVariables = Exact<{
 
 export type DefaultProfileQuery = { __typename?: 'Query', defaultProfile?: { __typename?: 'Profile', id: any, name?: string | null, bio?: string | null, isFollowedByMe: boolean, isFollowing: boolean, followNftAddress?: any | null, metadata?: any | null, isDefault: boolean, handle: any, ownedBy: any, attributes?: Array<{ __typename?: 'Attribute', displayType?: string | null, traitType?: string | null, key: string, value: string }> | null, picture?: { __typename?: 'MediaSet', original: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null }, small?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null, medium?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null } | { __typename?: 'NftImage', contractAddress: any, tokenId: string, uri: any, verified: boolean } | null, coverPicture?: { __typename?: 'MediaSet', original: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null }, small?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null, medium?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null } | { __typename?: 'NftImage', contractAddress: any, tokenId: string, uri: any, verified: boolean } | null, dispatcher?: { __typename?: 'Dispatcher', address: any, canUseRelay: boolean } | null, stats: { __typename?: 'ProfileStats', totalFollowers: number, totalFollowing: number, totalPosts: number, totalComments: number, totalMirrors: number, totalPublications: number, totalCollects: number }, followModule?: { __typename?: 'FeeFollowModuleSettings', type: FollowModules, recipient: any, amount: { __typename?: 'ModuleFeeAmount', value: string, asset: { __typename?: 'Erc20', name: string, symbol: string, decimals: number, address: any } } } | { __typename?: 'ProfileFollowModuleSettings', type: FollowModules, contractAddress: any } | { __typename?: 'RevertFollowModuleSettings', type: FollowModules, contractAddress: any } | { __typename?: 'UnknownFollowModuleSettings', type: FollowModules, contractAddress: any, followModuleReturnData: any } | null, onChainIdentity: { __typename?: 'OnChainIdentity', proofOfHumanity: boolean, ens?: { __typename?: 'EnsOnChainIdentity', name?: any | null } | null, sybilDotOrg: { __typename?: 'SybilDotOrgIdentity', verified: boolean, source: { __typename?: 'SybilDotOrgIdentitySource', twitter: { __typename?: 'SybilDotOrgTwitterIdentity', handle?: string | null } } }, worldcoin: { __typename?: 'WorldcoinIdentity', isHuman: boolean } } } | null };
 
+export type ProfileQueryVariables = Exact<{
+  request: SingleProfileQueryRequest;
+}>;
+
+
+export type ProfileQuery = { __typename?: 'Query', profile?: { __typename?: 'Profile', id: any, name?: string | null, bio?: string | null, isFollowedByMe: boolean, isFollowing: boolean, followNftAddress?: any | null, metadata?: any | null, isDefault: boolean, handle: any, ownedBy: any, attributes?: Array<{ __typename?: 'Attribute', displayType?: string | null, traitType?: string | null, key: string, value: string }> | null, picture?: { __typename?: 'MediaSet', original: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null }, small?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null, medium?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null } | { __typename?: 'NftImage', contractAddress: any, tokenId: string, uri: any, verified: boolean } | null, coverPicture?: { __typename?: 'MediaSet', original: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null }, small?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null, medium?: { __typename?: 'Media', url: any, width?: number | null, height?: number | null, mimeType?: any | null } | null } | { __typename?: 'NftImage', contractAddress: any, tokenId: string, uri: any, verified: boolean } | null, dispatcher?: { __typename?: 'Dispatcher', address: any, canUseRelay: boolean } | null, stats: { __typename?: 'ProfileStats', totalFollowers: number, totalFollowing: number, totalPosts: number, totalComments: number, totalMirrors: number, totalPublications: number, totalCollects: number }, followModule?: { __typename?: 'FeeFollowModuleSettings', type: FollowModules, recipient: any, amount: { __typename?: 'ModuleFeeAmount', value: string, asset: { __typename?: 'Erc20', name: string, symbol: string, decimals: number, address: any } } } | { __typename?: 'ProfileFollowModuleSettings', type: FollowModules, contractAddress: any } | { __typename?: 'RevertFollowModuleSettings', type: FollowModules, contractAddress: any } | { __typename?: 'UnknownFollowModuleSettings', type: FollowModules, contractAddress: any, followModuleReturnData: any } | null, onChainIdentity: { __typename?: 'OnChainIdentity', proofOfHumanity: boolean, ens?: { __typename?: 'EnsOnChainIdentity', name?: any | null } | null, sybilDotOrg: { __typename?: 'SybilDotOrgIdentity', verified: boolean, source: { __typename?: 'SybilDotOrgIdentitySource', twitter: { __typename?: 'SybilDotOrgTwitterIdentity', handle?: string | null } } }, worldcoin: { __typename?: 'WorldcoinIdentity', isHuman: boolean } } } | null };
+
 export type RecommendedProfilesQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -5530,6 +5537,27 @@ export const useDefaultProfileQuery = <
     useQuery<DefaultProfileQuery, TError, TData>(
       ['defaultProfile', variables],
       fetcher<DefaultProfileQuery, DefaultProfileQueryVariables>(DefaultProfileDocument, variables),
+      options
+    );
+export const ProfileDocument = `
+    query profile($request: SingleProfileQueryRequest!) {
+  profile(request: $request) {
+    ...ProfileFields
+  }
+}
+    ${ProfileFieldsFragmentDoc}
+${MediaFieldsFragmentDoc}
+${FollowModuleFieldsFragmentDoc}`;
+export const useProfileQuery = <
+      TData = ProfileQuery,
+      TError = unknown
+    >(
+      variables: ProfileQueryVariables,
+      options?: UseQueryOptions<ProfileQuery, TError, TData>
+    ) =>
+    useQuery<ProfileQuery, TError, TData>(
+      ['profile', variables],
+      fetcher<ProfileQuery, ProfileQueryVariables>(ProfileDocument, variables),
       options
     );
 export const RecommendedProfilesDocument = `
